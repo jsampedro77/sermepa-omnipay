371e9a4de1316f2e06d49d92ad1093be3bc2ac24
Ramón Muñiz <ramon@nitsnets.com>
2020-11-30 17:27:26 +0100

    first version of 3ds2 implementation

--- src/Message/Purchase3dsRequest.php
+++ src/Message/Purchase3dsRequest.php
@@ -13,121 +13,8 @@ use \Money\Currency;
  * @author Javier Sampedro <jsampedro77@gmail.com>
  * @author NitsNets Studio <github@nitsnets.com>
  */
-class PurchaseRequest extends AbstractRequest
+class Purchase3dsRequest extends PurchaseRequest
 {
-    protected $liveEndpoint = 'https://sis.redsys.es';
-    protected $testEndpoint = 'https://sis-t.redsys.es:25443';
-
-    /**
-     * @param $order
-     * @return PurchaseRequest
-     */
-    public function setOrder($order)
-    {
-        return $this->setParameter('order', $order);
-    }
-
-    /**
-     * @param $titular
-     * @return PurchaseRequest
-     */
-    public function setTitular($titular)
-    {
-        return $this->setParameter('titular', $titular);
-    }
-
-    /**
-     * @param $consumerLanguage
-     * @return PurchaseRequest
-     */
-    public function setConsumerLanguage($consumerLanguage)
-    {
-        return $this->setParameter('consumerLanguage', $consumerLanguage);
-    }
-
-    /**
-     * @param $merchantCode
-     * @return PurchaseRequest
-     */
-    public function setMerchantCode($merchantCode)
-    {
-        return $this->setParameter('merchantCode', $merchantCode);
-    }
-
-    /**
-     * @param $merchantName
-     * @return PurchaseRequest
-     */
-    public function setMerchantName($merchantName)
-    {
-        return $this->setParameter('merchantName', $merchantName);
-    }
-
-    /**
-     * @param $merchantURL
-     * @return PurchaseRequest
-     */
-    public function setMerchantURL($merchantURL)
-    {
-        return $this->setParameter('merchantURL', $merchantURL);
-    }
-
-    /**
-     * @param $merchantKey
-     */
-    public function setMerchantKey($merchantKey)
-    {
-        $this->setParameter('merchantKey', $merchantKey);
-    }
-
-    /**
-     * @param $terminal
-     * @return PurchaseRequest
-     */
-    public function setTerminal($terminal)
-    {
-        return $this->setParameter('terminal', $terminal);
-    }
-
-    /**
-     * Sets the identifier on the purchase request.
-     *
-     * @param string $identifier Identifier to be set on the purchase request
-     *
-     * @return object
-     */
-    public function setIdentifier($identifier)
-    {
-        return $this->setParameter('identifier', $identifier);
-    }
-
-    /**
-     * Gets the identifier parameter setup on the purchase request.
-     *
-     * @return mixed
-     */
-    public function getIdentifier()
-    {
-        return $this->getParameter('identifier');
-    }
-
-    /**
-     * @param $signatureMode
-     */
-    public function setSignatureMode($signatureMode)
-    {
-        $this->setParameter('signatureMode', $signatureMode);
-    }
-
-    /**
-     * @param $multiply
-     * @return PurchaseRequest
-     */
-    public function setMultiply($multiply)
-    {
-        return $this->setParameter('multiply', $multiply);
-    }
-
     /**
      * @return array|mixed
      * @throws \Omnipay\Common\Exception\InvalidRequestException
@@ -155,6 +42,8 @@ class PurchaseRequest extends AbstractRequest
         if (!empty($this->getParameter('identifier'))) {
             $data['Ds_Merchant_Identifier'] = $this->getParameter('identifier');
         }
+        
+        $data['DS_MERCHANT_EMV3DS'] = $this->get3dsParameters();
 
         $merchantParameters = base64_encode(json_encode($data));
 
@@ -164,85 +53,87 @@ class PurchaseRequest extends AbstractRequest
             'Ds_SignatureVersion' => 'HMAC_SHA256_V1'
         ];
     }
-
+    
+    
     /**
-     * @return float|int|string
-     * @throws \Omnipay\Common\Exception\InvalidRequestException
+     *
+     * @return array
      */
-    public function getAmount()
+    protected function get3dsParameters()
     {
-        if ($this->getParameter('multiply')) {
-            return strval((float)parent::getAmount() * 100);
-        }
-
-        return strval((float)parent::getAmount());
-    }
-
-    /**
-     * @return string
-     */
-    public function getTransactionId()
-    {
-        if (!empty(parent::getTransactionId())) {
-            return parent::getTransactionId();
-        }
-
-        return parent::getToken();
-    }
-
-    /**
-     * @return string
-     */
-    public function getTransactionType()
-    {
-        return '0';
-    }
-
-    /**
-     * @param $merchantParameters
-     * @return string
-     */
-    protected function generateSignature($merchantParameters)
-    {
-        $key = base64_decode($this->getParameter('merchantKey'));
-        $key = Encryptor::encrypt_3DES($this->getTransactionId(), $key);
-        $res = hash_hmac('sha256', $merchantParameters, $key, true);
-
-        return base64_encode($res);
-    }
-
-    /**
-     * @param mixed $data
-     * @return \Omnipay\Common\Message\ResponseInterface|PurchaseResponse
-     */
-    public function sendData($data)
-    {
-        return $this->response = new PurchaseResponse($this, $data);
-    }
-
-    /**
-     * @return string
-     */
-    public function getEndpoint()
-    {
-        return $this->getEndpointBase() . '/sis/realizarPago';
-    }
-
-    /**
-     * @return string
-     */
-    public function getEndpointBase()
-    {
-        return $this->getTestMode() ? $this->testEndpoint : $this->liveEndpoint;
-    }
-
-    /**
-     * @return int
-     */
-    private function getCurrencyRedsys()
-    {
-        $currencies = new ISOCurrencies();
-
-        return $currencies->numericCodeFor(new Currency($this->getCurrency()));
+        $data = [];
+        $data['cardholderName'] = substr($this->getParameter('cardholderName'), 0, 45);
+        $data['email'] = substr($this->getParameter('email'), 0, 45);
+        $data['homePhone'] = [
+            "cc" => substr($this->getParameter('homePhonePrefix'), 0, 3),
+            "subscriber" => substr($this->getParameter('homePhone'), 0, 15)
+        ];
+        $data['mobilePhone'] = [
+            "cc" => substr($this->getParameter('mobilePhonePrefix'), 0, 3),
+            "subscriber" => substr($this->getParameter('mobilePhone'), 0, 15)
+        ];
+        $data['workPhone'] = [
+            "cc" => substr($this->getParameter('mobilePhonePrefix'), 0, 3),
+            "subscriber" => substr($this->getParameter('mobilePhone'), 0, 15)
+        ];
+    
+        $data['shipAddrLine1'] = substr($this->getParameter('shipAddrLine1'), 0, 50);
+        $data['shipAddrLine2'] = substr($this->getParameter('shipAddrLine2'), 0, 50);
+        $data['shipAddrLine3'] = substr($this->getParameter('shipAddrLine3'), 0, 50);
+        $data['shipAddrCity'] = substr($this->getParameter('shipAddrCity'), 0, 50);
+        $data['shipAddrPostCode'] = substr($this->getParameter('shipAddrPostCode'), 0, 16);
+        $data['shipAddrState'] = substr($this->getParameter('shipAddrState'), 0, 3);
+        $data['shipAddrCountry'] = substr($this->getParameter('shipAddrCountry'), 0, 3);
+    
+        $data['billAddrLine1'] = substr($this->getParameter('billAddrLine1'), 0, 50);
+        $data['billAddrLine2'] = substr($this->getParameter('billAddrLine2'), 0, 50);
+        $data['billAddrLine3'] = substr($this->getParameter('billAddrLine3'), 0, 50);
+        $data['billAddrCity'] = substr($this->getParameter('billAddrCity'), 0, 50);
+        $data['billAddrPostCode'] = substr($this->getParameter('billAddrPostCode'), 0, 16);
+        $data['billAddrState'] = substr($this->getParameter('billAddrState'), 0, 3);
+        $data['billAddrCountry'] = substr($this->getParameter('billAddrCountry'), 0, 3);
+        
+        $data['addrMatch'] = $this->getParameter('addrMatch');
+        
+        $data['challengeWindowSize'] = $this->getParameter('challengeWindowSize');
+        
+        $data['acctID'] = substr($this->getParameter('acctID'), 0, 3);
+        $data['threeDSRequestorAuthenticationInfo'] = [
+            "threeDSReqAuthData" => '',
+            "threeDSReqAuthMethod" => $this->getParameter('threeDSReqAuthMethod'),
+            "threeDSReqAuthTimestamp" => $this->getParameter('threeDSReqAuthTimestamp'),
+        ];
+        
+        $data['acctInfo'] = [
+            "chAccAgeInd" => $this->getParameter('chAccAgeInd'),
+            "chAccDate" => $this->getParameter('chAccDate'),
+            "chAccChange" => $this->getParameter('chAccChange'),
+            "chAccChangeInd" => $this->getParameter('chAccChangeInd'),
+            "chAccPwChange" => $this->getParameter('chAccPwChange'),
+            "chAccPwChangeInd" => $this->getParameter('chAccPwChangeInd'),
+            "nbPurchaseAccount" => $this->getParameter('nbPurchaseAccount'),
+            "provisionAttemptsDay" => $this->getParameter('provisionAttemptsDay'),
+            "txnActivityDay" => $this->getParameter('txnActivityDay'),
+            "txnActivityYear" => $this->getParameter('txnActivityYear'),
+            "paymentAccAge" => $this->getParameter('paymentAccAge'),
+            "paymentAccInd" => $this->getParameter('paymentAccInd'),
+            "shipAddressUsage" => $this->getParameter('shipAddressUsage'),
+            "shipAddressUsageInd" => $this->getParameter('shipAddressUsageInd'),
+            "shipNameIndicator" => $this->getParameter('shipNameIndicator'),
+            'suspiciousAccActivity' => $this->getParameter('suspiciousAccActivity'),
+        ];
+        $data['merchantRiskIndicator'] = [
+            "deliveryEmailAddress" => $this->getParameter('deliveryEmailAddress'),
+            "deliveryTimeframe" => $this->getParameter('deliveryTimeframe'),
+            "giftCardAmount" => $this->getParameter('giftCardAmount'),
+            "giftCardCount" => $this->getParameter('giftCardCount'),
+            "giftCardCurr" => $this->getParameter('giftCardCurr'),
+            "preOrderDate" => $this->getParameter('preOrderDate'),
+            "preOrderPurchaseInd" => $this->getParameter('preOrderPurchaseInd'),
+            "reorderItemsInd" => $this->getParameter('reorderItemsInd'),
+            "shipIndicator" => $this->getParameter('shipIndicator'),
+            
+        ];
+        return $data;
     }
 }
